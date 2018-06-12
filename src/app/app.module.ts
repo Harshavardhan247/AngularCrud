3c0d550a6028f4af33f95029b2808b5008d1dfa3
harshavardhan Reddy <o.harshavardhan@gmail.com>
2018-06-12 07:48:48 -0400

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { ListEmployeesComponent } from './employees/list-employees.component';
+import { CreateEmployeeComponent } from './employees/create-employee.component';
+
+const appRoutes: Routes = [
+  {path: 'list', component: ListEmployeesComponent },
+  {path: 'create', component: CreateEmployeeComponent },
+
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListEmployeesComponent
+    ListEmployeesComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule
